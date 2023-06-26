3cb55f96e7611ae0b1ec93140dd45e4e8daf85be
Leechael Yim <yanleech@gmail.com>
2023-06-26 22:52:27 +0800

    feat: AnimatedDetails

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,10 +1,8 @@
 'use client';
 
-import type { ReactNode } from "react"
-import type { PrimitiveAtom } from "jotai"
-
-import { useMemo } from 'react'
-import { atom, useAtom } from "jotai"
+import { type ReactNode, useState, useMemo, useEffect } from 'react'
+import { type PrimitiveAtom, atom, useAtom } from "jotai"
+import { motion } from 'framer-motion'
 
 import { cn } from '@/lib/utils'
 
@@ -33,6 +31,66 @@ export default function Details({
       }}
     >
       {children}
+    </details>
+  )
+}
+
+export function AnimatedDetails({
+  theIdxAtom,
+  idx,
+  summary,
+  summaryClassName,
+  children,
+  ...props
+}: {
+  theIdxAtom?: PrimitiveAtom<number>
+  idx: number
+  summary: ReactNode
+  summaryClassName?: string
+  children: ReactNode
+} & React.HTMLProps<HTMLDetailsElement>) {
+  const [isClosed, setIsClosed] = useState(false)
+  const theAtom = useMemo(() => theIdxAtom || defaultOpenedDetailIdxAtom, [theIdxAtom])
+  const [currentIdx, setCurrentIdx] = useAtom(theAtom)
+  useEffect(() => {
+    if (currentIdx !== idx) {
+      setIsClosed(true)
+    } else {
+      setIsClosed(false)
+    }
+  }, [currentIdx, setCurrentIdx, idx])
+  return (
+    <details
+      {...props}
+      className={cn(props.className, idx === currentIdx ? 'active' : null)}
+      open
+      onClick={(ev) => {
+        ev.preventDefault()
+        if ((ev.target as HTMLElement).tagName === 'SUMMARY' && ev.currentTarget.open) {
+          setIsClosed(i => !i)
+          setCurrentIdx(idx)
+        }
+      }}
+    >
+      <summary
+        className={summaryClassName}
+        onClick={ev => {
+          ev.preventDefault()
+        }}
+      >
+        {summary}
+      </summary>
+      <motion.div
+        className={cn("overflow-hidden")}
+        initial={{ height: 0, scale: 0.75, opacity: 0.25 }}
+        animate={{
+          height: isClosed ? 0 : 'auto',
+          scale: isClosed ? 0.75 : 1,
+          opacity: isClosed ? 0.25 : 1,
+        }}
+      >
+        {children}
+      </motion.div>
     </details>
   )
 }
