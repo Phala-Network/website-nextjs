4c7dba3679bacffd6a22ac0bdc45c58ae5cfd0e2
Leechael Yim <yanleech@gmail.com>
2023-06-28 04:12:49 +0800

    chore: more settings on next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  poweredByHeader: false,
+  trailingSlash: false,
+  basePath: '',
+  // The starter code load resources from `public` folder with `router.basePath` in React components.
+  // So, the source code is "basePath-ready".
+  // You can remove `basePath` if you don't need it.
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
