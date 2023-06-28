fe59859658f2060436308465f9b1f6cf8e0d9a22
Leechael Yim <yanleech@gmail.com>
2023-06-29 03:10:01 +0800

    imp: UI adjustment.

--- src/app/_components/Advantages.tsx
+++ src/app/_components/Advantages.tsx
@@ -25,18 +25,22 @@ export function Advantages() {
   }, [setIndex])
   return (
     <AnimatePresence mode="sync">
-      <span className={cn("ml-[0.5ch] relative h-8")} style={{ width: words[index][0].length + 'ch' }}>
+      <span className={cn("advantages", "ml-[0.5ch] relative h-8 font-mono")} style={{ width: words[index][0].length + 'ch' }}>
         {words.map(([word, color], i) => (
           <motion.span
             className={cn("absolute left-0")}
             key={word}
-            initial={{ opacity: 0, y: -20, color: 'rgba(255, 255, 255, 0)' }}
-            animate={{ opacity: i === index ? 1 : 0, y: i === index ? 0 : 20, color }}
-            exit={{ opacity: 0, y: -20, color: 'rgba(255, 255, 255, 0)' }}
+            style={{
+              textShadow: `10px 0px 3px ${color}, 16px 3px 9px ${color}, 2px 0px 6px ${color}, -12px 0px 12px ${color}`,
+            }}
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: i === index ? 1 : 0, y: i === index ? 0.5 : 20 }}
+            exit={{ opacity: 0, y: -20 }}
           >
             {word}
           </motion.span>
         ))}
+      <span className="absolute top-0" style={{ right: `-1ch` }}>.</span>
       </span>
     </AnimatePresence>
   )
