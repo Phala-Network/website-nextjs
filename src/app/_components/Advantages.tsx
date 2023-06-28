daca47b4abd79cb5c9765309a0496e3534268d30
Leechael Yim <yanleech@gmail.com>
2023-06-29 03:36:20 +0800

    imp: color & text shadow.

--- src/app/_components/Advantages.tsx
+++ src/app/_components/Advantages.tsx
@@ -6,13 +6,13 @@ import { motion, AnimatePresence } from 'framer-motion'
 import { cn } from '@/lib/utils'
 
 const words = [
-  ['Capability', '#8544F6'],
-  ['Performance', '#ED67D7'],
-  ['HTTP', '#F24E7F'],
-  ['Automation', '#FFA14A'],
-  ['Compute', '#86F000'],
-  ['Potential', '#448BF6'],
-  ['Interoperability', '#00E9B1'],
+  ['Capability', '#8544F6', '9ch'],
+  ['Performance', '#F64DDB', '11.9ch'],
+  ['HTTP', '#FF3975', '3.8ch'],
+  ['Automation', '#FF9533', '10.5ch'],
+  ['Compute', '#75D100', '7.5ch'],
+  ['Potential', '#1D77FF', '8.5ch'],
+  ['Interoperability', '#00D3A1', '14.7ch'],
 ]
 
 export function Advantages() {
@@ -25,17 +25,14 @@ export function Advantages() {
   }, [setIndex])
   return (
     <AnimatePresence mode="sync">
-      <span className={cn("advantages", "ml-[0.5ch] relative h-8 font-mono")} style={{ width: words[index][0].length + 'ch' }}>
+      <span className={cn("advantages", "ml-[0.5ch] relative h-8")} style={{ width: words[index][2] }}>
         {words.map(([word, color], i) => (
           <motion.span
             className={cn("absolute left-0")}
             key={word}
-            style={{
-              textShadow: `10px 0px 3px ${color}, 16px 3px 9px ${color}, 2px 0px 6px ${color}, -12px 0px 12px ${color}`,
-            }}
-            initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: i === index ? 1 : 0, y: i === index ? 0.5 : 20 }}
-            exit={{ opacity: 0, y: -20 }}
+            initial={{ opacity: 0, y: -20, textShadow: 'rgba(255, 255, 255, 0)' }}
+            animate={{ opacity: i === index ? 1 : 0, y: i === index ? 0 : 20, color }}
+            exit={{ opacity: 0, y: -20, color: 'rgba(255, 255, 255, 0)' }}
           >
             {word}
           </motion.span>
