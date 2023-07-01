c8d841168c9ec1904910dfab948a73df988a3576
Leechael Yim <yanleech@gmail.com>
2023-07-02 00:18:31 +0800

    imp: setup cache control for the api route handler

--- src/app/api/computation-metrics/route.ts
+++ src/app/api/computation-metrics/route.ts
@@ -1,11 +1,15 @@
 import { getComputationMeta } from "@/lib/computationMeta"
 
+// It's ok cache for 5 minutes.
+export const revalidate = 300
+
 export async function GET() {
   const result = await getComputationMeta()
   return new Response(JSON.stringify(result), {
     status: 200,
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'Cache-Control': 'public,max-age=300,stale-while-revalidate',
     }
   })
 }
