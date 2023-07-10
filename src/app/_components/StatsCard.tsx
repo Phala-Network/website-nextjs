ffb3b1ee1c0e9a7b106b3ed4f5598d2cede940fe
Leechael Yim <yanleech@gmail.com>
2023-07-10 14:16:35 +0800

    imp: UI adjustment

--- src/app/_components/StatsCard.tsx
+++ src/app/_components/StatsCard.tsx
@@ -8,7 +8,7 @@ const formatter = new Intl.NumberFormat('en-US')
 
 function Stats({ children, name }: { children: ReactNode, name: string }) {
   return (
-    <div>
+    <div className={"flex flex-col items-center"}>
       <h4 className={"text-lg font-semibold"}>{children}</h4>
       <div className={"text-sm text-blackAlpha-700"}>{name}</div>
     </div>
@@ -18,7 +18,7 @@ function Stats({ children, name }: { children: ReactNode, name: string }) {
 export function StatsCard() {
   const stats = useComputationStats()
   return (
-    <div className={"flex flex-row gap-9"}>
+    <div className={"flex flex-row gap-12"}>
       <Stats name="Computers">{formatter.format(stats.totalNodes)}</Stats>
       <Stats name="$PHA">{formatter.format(stats.totalValue)}</Stats>
     </div>
