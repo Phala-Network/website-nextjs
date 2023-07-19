9052ef3c37a91ec715f751b9a4cf5d29ecfcf6fc
pacoyang <yangbaigao@gmail.com>
2023-07-19 23:44:42 +0800

    feat: animation of the how it works section

--- src/app/_components/SectionHowItWorks.tsx
+++ src/app/_components/SectionHowItWorks.tsx
@@ -1,25 +1,27 @@
 'use client'
 
+import { useState } from 'react'
 import { atom, useAtom, useAtomValue } from 'jotai'
 
 import { cn } from '@/lib/utils'
 import './SectionHowItWorks.css'
+import { WorkFlow } from './work_flow'
+import QuoteLine1 from './work_flow/svgs/quote_line1'
+import QuoteLine2 from './work_flow/svgs/quote_line2'
+import QuoteLine3 from './work_flow/svgs/quote_line3'
 
 const worksAtom = atom([
   {
     title: 'Correctness',
     content: 'Phat Contracts are deployed to the blockchain and assigned to the offchain workers, running inside Secure Enclaves. Anyone can check the signed transactions and Secure Enclave remote attestation to verify that the executed code is the one published on the blockchain.',
-    imageUrl: '/home/how-it-works-01.png'
   },
   {
     title: 'Replicated',
     content: 'Phat Contracts are deployed to one or more workers, grouped as a Cluster. The contracts in the same cluster are replicated to further guarantee the availability. Thanks to the redundancy by the Cluster design, the developer can further validate the states across the workers to...',
-    imageUrl: '/home/how-it-works-02.png'
   },
   {
     title: 'Confidentiality',
     content: 'Phala Network implemented end-to-end encryption during the full Phat Contract lifecycle. Secure Enclave acts as a two-way sandbox that encrypts input, output, and internet access, protecting the secrets in the enclave against software and hardware level attack.',
-    imageUrl: '/home/how-it-works-03.png'
   }
 ])
 
@@ -28,6 +30,13 @@ const activeWorkAtom = atom(0)
 export function SectionHowItWorks() {
   const works = useAtomValue(worksAtom)
   const [activeWork, setActiveWork] = useAtom(activeWorkAtom)
+  const [animation, setAnimation] = useState('')
+
+  const handleSwitch = (next: number) => {
+    setAnimation(`flow${activeWork + 1}to${next + 1}`)
+    setActiveWork(next)
+  }
+
   return (
     <section id="section-how-it-works" className="swiper-no-swiping">
       {/* Mobile */}
@@ -73,15 +82,11 @@ export function SectionHowItWorks() {
               <p className="mt-5">{work.content}</p>
               <div
                 className={cn(
-                  "w-full aspect-[343/280]",
-                  "mt-8 rounded-3xl",
-                  "bg-no-repeat bg-bottom",
+                  "w-full mt-10",
                 )}
-                style={{
-                  backgroundImage: `url("${work.imageUrl}")`,
-                  backgroundSize: '150%',
-                }}
-              />
+              >
+                <WorkFlow inViewAnimation={index === 1 ? 'flow1to2' : index === 2 ? 'flow1to3' : ''} />
+              </div>
             </div>
           ))
         }
@@ -94,68 +99,79 @@ export function SectionHowItWorks() {
           "mt-24",
         )}
       >
-        <h2 className={cn(
-          "font-black text-5xl uppercase"
-        )}>
-          How It Works
-        </h2>
-        <div
-          className={cn(
-            "mt-16",
-            "grid gap-8 grid-cols-3",
-            "cursor-pointer",
-          )}
-        >
-          {
-            works.map((work, index) => (
-              <div
-                key={index.toString()}
-                className={cn(
-                  "work-item",
-                  "flex flex-col",
-                  "transition-opacity duration-500",
-                  "hover:opacity-100",
-                  activeWork !== index ? 'opacity-50' : null
-                )}
-                onClick={() => setActiveWork(index)}
-              >
-                <h3
-                  className={cn(
-                    "font-semibold text-2xl uppercase",
-                    "flex items-center",
-                  )}
-                >
-                  {work.title}
-                  <img
-                    src="/icons/right-arrow.svg"
-                    alt=""
+        <div className="w-full grid grid-cols-20 3xl:grid-cols-24">
+          <div className="col-start-2 3xl:col-start-4 col-span-18 relative">
+            <h2 className={cn(
+              "font-black text-5xl uppercase"
+            )}>
+              How It Works
+            </h2>
+            <div
+              className={cn(
+                "mt-16",
+                "grid gap-8 grid-cols-3",
+                "cursor-pointer",
+              )}
+            >
+              {
+                works.map((work, index) => (
+                  <div
+                    key={index.toString()}
                     className={cn(
-                      "icon",
-                      "ml-2 inline-block w-6 h-6",
-                      "transition-opacity transition-transform duration-500 opacity-0 translate-x-0",
+                      "work-item",
+                      "flex flex-col",
+                      "transition-opacity duration-500",
+                      "hover:opacity-100",
+                      activeWork !== index ? 'opacity-50' : null
                     )}
-                  />
-                </h3>
-                <p className="mt-5">{work.content}</p>
-              </div>
-            ))
-          }
-        </div>
-        <div className="mt-5 w-full relative">
-          {
-            works.map((work, index) => (
-              <img
-                className={cn(
-                  "w-full",
-                  activeWork !== index ? 'opacity-30' : 'z-10',
-                  index > 0 ? 'absolute top-0 left-0' : 'relative'
-                )}
-                key={index.toString()}
-                src={work.imageUrl}
-                alt=""
+                    onClick={() => handleSwitch(index)}
+                  >
+                    <h3
+                      className={cn(
+                        "font-semibold text-2xl uppercase",
+                        "flex items-center",
+                      )}
+                    >
+                      {work.title}
+                      <img
+                        src="/icons/right-arrow.svg"
+                        alt=""
+                        className={cn(
+                          "icon",
+                          "ml-2 inline-block w-6 h-6",
+                          "transition-opacity transition-transform duration-500 opacity-0 translate-x-0",
+                        )}
+                      />
+                    </h3>
+                    <p className="mt-5">{work.content}</p>
+                  </div>
+                ))
+              }
+            </div>
+          </div>
+          <div className="col-start-2 3xl:col-start-4 col-span-18 relative">
+            <div className="relative top-[2.8%] -z-10">
+              <QuoteLine1
+                fill={activeWork === 0 ? '#171717' : '#EDEDF0'}
+                className={cn("w-[39.81%] relative left-[1%]", activeWork === 0 ? 'z-10' : null)}
               />
-            ))
-          }
+              <QuoteLine2
+                className={cn(
+                  "w-[12.27%] absolute top-0 left-[28.7%]",
+                  activeWork === 1 ? 'z-10' : null,
+                )}
+                fill={activeWork === 1 ? '#171717' : '#EDEDF0'}
+              />
+              <QuoteLine3
+                className={cn(
+                  "w-[39.81%] absolute top-0 left-[28.9%]",
+                  activeWork === 2 ? 'z-10' : null,
+                )}
+                fill={activeWork === 2 ? '#171717' : '#EDEDF0'}
+              />
+            </div>
+            <WorkFlow animation={animation} />
+          </div>
         </div>
       </div>
     </section>
---
+++ src/app/_components/work_flow/svgs/quote_line2.tsx
@@ -0,0 +1,25 @@
+import * as react from 'react'
+import { SVGProps } from 'react'
+
+const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 162 149"
+    {...props}
+    fill="none"
+  >
+    <path
+      stroke={props.fill}
+      strokeLinecap="round"
+      strokeWidth={4}
+      d="M83.5 10v106.5"
+    />
+    <circle cx={83} cy={9.5} r={9} fill={props.fill} stroke={props.fill} />
+    <path
+      fill={props.fill}
+      d="M57.497 124H.5v24.5H162V124h-52.5c-13.255 0-24-10.745-24-24v-6h-4.003v6c0 13.255-10.745 24-24 24Z"
+    />
+  </svg>
+)
+export default SvgComponent
+
