7c79e9590fd25a96d356abf6c5a313a8033974e6
pacoyang <yangbaigao@gmail.com>
2023-07-20 13:03:18 +0800

    refactor: add animation for workflow switch

--- src/app/_components/workflow/svgs/quote_line3.tsx
+++ src/app/_components/workflow/svgs/quote_line3.tsx
@@ -5,18 +5,18 @@ const SvgComponent = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 536 149"
-    {...props}
     fill="none"
+    {...props}
   >
-    <circle cx={526} cy={9.5} r={9} fill={props.fill} stroke={props.fill} />
+    <circle cx={526} cy={9.5} r={9} fill="#171717" stroke="#171717" />
     <path
-      stroke={props.fill}
+      stroke="#171717"
       strokeLinecap="round"
       strokeWidth={4}
       d="M83.5 114V92c0-17.673 14.327-32 32-32H494c17.673 0 32-14.327 32-32V10.5"
     />
     <path
-      fill={props.fill}
+      fill="#171717"
       d="M57.497 124H.5v24.5H162V124h-52.5c-13.255 0-24-10.745-24-24v-6h-4.003v6c0 13.255-10.745 24-24 24Z"
     />
   </svg>
