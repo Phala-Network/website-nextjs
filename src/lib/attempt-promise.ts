0990ceb46663302ea8cd303f11905485d6bbfb6c
pacoyang <yangbaigao@gmail.com>
2023-08-03 11:34:36 +0800

    feat: render notion page

---
+++ src/lib/attempt-promise.ts
@@ -0,0 +1,23 @@
+async function attempt<T>(promise: Promise<T>): Promise<[Error | undefined, T]> {
+  let error: Error | undefined
+  let result
+  try {
+    result = await promise
+  } catch (e) {
+    error = e as Error
+  }
+  return [error, result as T]
+}
+
+attempt.all = async <T>(promises: Promise<T>[]): Promise<[Error[], T[]]> => {
+  const ret = await Promise.all(promises.map((promise) => attempt(promise)))
+  const errors: Error[] = []
+  const results: T[] = []
+  ret.forEach(([error, result]) => {
+    if (error) errors.push(error)
+    results.push(result)
+  })
+  return [errors, results]
+}
+
+export default attempt
