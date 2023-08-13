9f225b308fa6c1849ac8432a0e9c16d06c7e5cf7
pacoyang <yangbaigao@gmail.com>
2023-08-13 21:17:31 +0800

    feat: integrate notion api

--- src/pages/components/Banners.tsx
+++ src/pages/components/Banners.tsx
@@ -1,70 +1,79 @@
 import { CSSProperties } from 'react'
+import dayjs from 'dayjs'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Pagination } from 'swiper/modules'
 import 'swiper/css/pagination'
 
 import { cn } from '@/lib/utils'
-import Tag from '@/pages/components/Tag'
+import TagLink from '@/pages/components/TagLink'
+import { ParsedListPage } from '@/lib/notion-client'
 
-export default function Banners() {
-  const items = [1, 2]
+export default function Banners({ pages }: { pages: ParsedListPage[] }) {
   return (
     <div
       className="w-full h-full"
-      style={{
-        '--swiper-pagination-color': '#CDFA50',
-        '--swiper-pagination-bullet-inactive-color': '#FFFFFF',
-        '--swiper-pagination-bullet-inactive-opacity': 1
-      } as CSSProperties}
+      style={
+        {
+          '--swiper-pagination-color': '#CDFA50',
+          '--swiper-pagination-bullet-inactive-color': '#FFFFFF',
+          '--swiper-pagination-bullet-inactive-opacity': 1,
+        } as CSSProperties
+      }
     >
       <Swiper
-        className={cn(
-          "w-full h-full",
-          "rounded-3xl",
-        )}
+        className={cn('w-full h-full', 'rounded-3xl')}
         modules={[Pagination]}
         spaceBetween={30}
         pagination={{ clickable: true, el: '.banners-pagination' }}
       >
-        {items.map((i) => (
+        {pages.map((page) => (
           <SwiperSlide
-            key={i}
+            key={page.id}
             className={cn(
-              "aspect-[500/400] lg:aspect-[1316/531]",
-              "w-full h-full bg-[#FAFEED]",
-              "rounded-3xl p-2",
+              'aspect-[500/400] lg:aspect-[1316/531]',
+              'w-full h-full bg-[#FAFEED]',
+              'rounded-3xl p-2'
             )}
           >
-            <article className={cn(
-              "w-full",
-              "grid grid-cols-1 lg:grid-cols-20"
-            )}>
-              <div className={cn(
-                "lg:col-span-12",
-                "aspect-[500/400]",
-                "rounded-3xl overflow-hidden",
-              )}>
-                <img
-                  className="w-full h-full object-cover"
-                  src="/home/highlight05.jpg"
-                  alt=""
-                />
+            <article
+              className={cn('w-full', 'grid grid-cols-1 lg:grid-cols-20')}
+            >
+              <div
+                className={cn(
+                  'lg:col-span-12',
+                  'aspect-[500/400]',
+                  'rounded-3xl overflow-hidden'
+                )}
+              >
+                <a href={`/posts${page.slug}`}>
+                  <img
+                    className="w-full h-full object-cover"
+                    src={page.coverUrl}
+                    alt={page.title}
+                  />
+                </a>
               </div>
-              <div className={cn(
-                "lg:col-span-8",
-                "flex flex-col justify-between",
-                "p-4"
-              )}>
+              <div
+                className={cn(
+                  'lg:col-span-8',
+                  'flex flex-col justify-between',
+                  'p-4'
+                )}
+              >
                 <div className="flex flex-col gap-y-2">
                   <div className="flex items-center gap-x-4">
-                    <Tag>Weekly report</Tag>
+                    {page.tags.map((tag, i) => (
+                      <TagLink key={`${i}`} href={`/tags/${tag}`}>
+                        {tag}
+                      </TagLink>
+                    ))}
                   </div>
-                  <h2 className="swiper-no-swiping font-bold text-2xl">Phala Builders Program Launched! PW Sent Out Survey For Community | Phala Weekly Vol. 120</h2>
+                  <h2 className="swiper-no-swiping font-bold text-2xl">
+                    <a href={`/posts${page.slug}`}>{page.title}</a>
+                  </h2>
                 </div>
                 <div className="text-sm">
-                  <p>
-                    2023-02-09
-                  </p>
+                  <p>{dayjs(page.createdTime).format('YYYY-MM-DD')}</p>
                 </div>
               </div>
             </article>
