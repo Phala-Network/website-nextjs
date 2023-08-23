28744e0ab213447419367a59727f0542afaa1991
pacoyang <yangbaigao@gmail.com>
2023-08-23 14:46:58 +0800

    refactor: use published time instead of created time

--- src/pages/api/posts.ts
+++ src/pages/api/posts.ts
@@ -37,7 +37,7 @@ export default async function handler(
     filter,
     sorts: [
       {
-        timestamp: 'created_time',
+        property: 'Published Time',
         direction: 'descending',
       },
     ],
