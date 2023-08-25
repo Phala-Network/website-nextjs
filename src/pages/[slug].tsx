0edb74a334679fa4d73e82dbb06b1336d4908ea7
Leechael Yim <yanleech@gmail.com>
2023-08-26 00:47:38 +0800

    feat: rendering custom page.

---
+++ src/pages/[slug].tsx
@@ -0,0 +1,162 @@
+import React, { AnchorHTMLAttributes } from 'react'
+import type { GetStaticProps } from 'next'
+import Head from 'next/head'
+import { ParsedUrlQuery } from 'querystring'
+import { useHydrateAtoms } from 'jotai/utils'
+import { FiArrowLeft, FiArrowRight } from 'react-icons/fi'
+import dayjs from 'dayjs'
+
+import { cn } from '@/lib/utils'
+import attempt from '@/lib/attempt-promise'
+import {
+  queryDatabase,
+  getParsedPagesByProperties,
+  ParsedPage,
+  ParsedListPage,
+} from '@/lib/notion-client'
+import { render_block } from '@/components/notion-render/Block'
+import { blocksAtom } from '@/components/notion-render/atoms'
+import TagLink from '@/pages/components/TagLink'
+import SectionSubscription from '@/pages/components/SectionSubscription'
+import '@/components/notion-render/styles.css'
+
+interface Props {
+  page: ParsedPage | null
+  recentPages?: ParsedListPage[]
+  similarPages?: ParsedListPage[]
+  beforePages?: ParsedListPage[]
+  nextPages?: ParsedListPage[]
+}
+
+function AboutLink({
+  children,
+  ...props
+}: AnchorHTMLAttributes<HTMLAnchorElement>) {
+  return (
+    <a
+      className="text-center text-xs text-green-700 font-bold leading-none border-r-[1px] border-green-700 w-[136px]"
+      {...props}
+    >
+      {children}
+    </a>
+  )
+}
+
+const StaticPage = ({ page }: Props) => {
+  if (!page) {
+    return (
+      <div className="max-w-3xl m-auto p-8">
+        <p>Woops! didn't find that post.</p>
+      </div>
+    )
+  }
+  useHydrateAtoms([[blocksAtom, page.blocks]])
+  return (
+    <>
+      {page.title ? (
+        <Head>
+          <title>{page.title}</title>
+          {page.status !== 'Published' ? (
+          <meta name="robots" content="noindex" />
+          ) : null}
+        </Head>
+      ) : null}
+      <div
+        className={cn(
+          'min-h-screen',
+          'bg-gradient-to-b from-green-600 to-green-500'
+        )}
+      >
+        <div
+          className={cn(
+            'safe-viewport',
+            'grid grid-cols-1 lg:grid-cols-20 3xl:grid-cols-24 gap-4',
+            'py-32'
+          )}
+        >
+          <div
+            className={cn(
+              'col-start-1 lg:col-span-18 lg:col-start-2 3xl:col-start-4 3xl:col-span-18'
+            )}
+          >
+            <article
+              className={cn(
+                'notion_page_body',
+                'bg-white rounded-3xl p-2 mt-4'
+              )}
+            >
+              {page.coverUrl ? (
+                <div
+                  className={cn('aspect-[856/442] rounded-3xl overflow-hidden')}
+                >
+                  <img
+                    className="w-full object-contain"
+                    src={page.coverUrl}
+                    alt=""
+                  />
+                </div>
+              ) : null}
+              <div className="p-8 pt-0">
+                <h1 className={cn('notion_page_title', 'text-3xl font-black')}>
+                  {page.title}
+                </h1>
+                <div className="text-base">{page.blocks.map(render_block)}</div>
+              </div>
+            </article>
+          </div>
+        </div>
+        <SectionSubscription />
+      </div>
+    </>
+  )
+}
+
+interface Params extends ParsedUrlQuery {
+  slug: string
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      {
+        params: {
+          slug: '/privacy',
+        },
+      },
+    ],
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
+  const slug = params!.slug as string
+  const [error, pages] = await attempt(
+    getParsedPagesByProperties({
+      database_id: process.env.NOTION_POSTS_DATABASE_ID!,
+      properties: {
+        'Custom URL': slug,
+      },
+    })
+  )
+  if (error) {
+    console.error(error)
+  }
+  if (pages.length === 0) {
+    return {
+      props: {
+        page: null,
+        recentPages: [],
+        similarPages: [],
+      },
+    }
+  }
+  const page = pages[0]
+  return {
+    props: {
+      page,
+    },
+    revalidate: 7200,
+  }
+}
+
+export default StaticPage
