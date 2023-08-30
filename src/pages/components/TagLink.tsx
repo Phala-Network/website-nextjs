d380b418dfb94bf90ddcbce0224a5b46580c6708
Leechael Yim <yanleech@gmail.com>
2023-08-30 17:21:21 +0800

    imp: prefer Link instead a element

--- src/pages/components/TagLink.tsx
+++ src/pages/components/TagLink.tsx
@@ -1,15 +1,16 @@
-import React, { AnchorHTMLAttributes } from 'react'
+import React, { type ReactNode } from 'react'
+import Link, { type LinkProps } from 'next/link'
 
 export default function TagLink({
   children,
   ...props
-}: AnchorHTMLAttributes<HTMLAnchorElement>) {
+}: LinkProps & { children: ReactNode }) {
   return (
-    <a
-      className="text-xs text-black-800 bg-green-500 rounded-md px-3 py-1"
+    <Link
+      className="text-xs text-black-800 bg-green-500 rounded-md px-3 py-1 transition-all hover:bg-green-600"
       {...props}
     >
       {children}
-    </a>
+    </Link>
   )
 }
