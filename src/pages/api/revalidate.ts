d4a7847a0ff1897b3fd8600ef083d45960e42603
pacoyang <yangbaigao@gmail.com>
2023-09-06 17:19:45 +0800

    refactor: move all apis to app routers

--- src/pages/api/revalidate.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiResponse, NextApiRequest } from 'next'
-
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  await res.revalidate(req.body.slug)
-  return res.json({ revalidated: true })
-}
