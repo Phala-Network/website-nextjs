f7061590899a4a55e05f5a255e391010caee8197
pacoyang <yangbaigao@gmail.com>
2023-09-06 15:55:03 +0800

    refactor: avoid using hooks in conditional statements

--- src/hooks/useProxyImage.ts
+++ src/hooks/useProxyImage.ts
@@ -1,29 +1,31 @@
 import { useState, useEffect } from 'react'
 
 export default function useProxyImage({
-  url,
+  image,
   block_id = '',
   page_id = '',
 }: {
-  url: string
+  image: { url: string; type: 'file' | 'external' }
   block_id?: string
   page_id?: string
 }) {
-  const [proxyUrl, setProxyUrl] = useState(url)
+  const [proxyUrl, setProxyUrl] = useState(image.url)
   useEffect(() => {
     ;(async () => {
-      const params = new URLSearchParams()
-      params.append('url', url)
-      if (block_id) {
-        params.append('block_id', block_id)
+      if (image.type === 'file') {
+        const params = new URLSearchParams()
+        params.append('url', image.url)
+        if (block_id) {
+          params.append('block_id', block_id)
+        }
+        if (page_id) {
+          params.append('page_id', page_id)
+        }
+        const res = await fetch(`/api/image?${params.toString()}`)
+        const data = await res.json()
+        setProxyUrl(data.url)
       }
-      if (page_id) {
-        params.append('page_id', page_id)
-      }
-      const res = await fetch(`/api/image?${params.toString()}`)
-      const data = await res.json()
-      setProxyUrl(data.url)
     })()
-  }, [url])
+  }, [image])
   return proxyUrl
 }
