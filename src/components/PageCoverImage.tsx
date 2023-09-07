364c9fcffeb62c10706b847e7f0bfba9e6c916e0
pacoyang <yangbaigao@gmail.com>
2023-09-07 13:33:36 +0800

    refactor: proxy image hooks

--- src/components/PageCoverImage.tsx
+++ src/components/PageCoverImage.tsx
@@ -3,18 +3,12 @@ import {
   type ParsedPage,
   type ParsedListPage,
 } from '@/lib/notion-client'
-import useProxyImage from '@/hooks/useProxyImage'
+import { useProxyPageImage } from '@/hooks/useProxyImage'
 
 export default function PageCoverImage({
   page,
   ...props
 }: React.HTMLProps<HTMLImageElement> & { page: ParsedPage | ParsedListPage }) {
-  const coverUrl = useProxyImage({
-    image: {
-      type: page.cover ? page.cover.type : 'external',
-      url: page.cover ? page.cover.type == 'external' ? removeMediumFormat(page.cover.external.url) : page.cover.file.url : '',
-    },
-    page_id: page.id
-  })
+  const coverUrl = useProxyPageImage(page)
   return <img {...props} src={coverUrl} alt={page.title} />
 }
