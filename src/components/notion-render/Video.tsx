a85b2a2567bf7f9821f34bcf55f43e6c9ef39dbb
Leechael Yim <yanleech@gmail.com>
2023-09-17 03:55:47 +0800

    fix: video block not rendering and you LiteYouTubeEmebed for youtube video

--- src/components/notion-render/Video.tsx
+++ src/components/notion-render/Video.tsx
@@ -1,34 +1,34 @@
+import { useMemo } from 'react'
 import { useAtomValue } from 'jotai'
 import { VideoBlockObjectResponse } from '@notionhq/client/build/src/api-endpoints'
+import LiteYouTubeEmbed from 'react-lite-youtube-embed'
+import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css'
 
 import { BlockAtom } from './atoms'
 import RichText from './RichText'
 
-const YoutubeEmbed = ({ embedId }: { embedId?: string })  => (
-  <iframe
-    className="w-full aspect-[853/480]"
-    src={`https://www.youtube.com/embed/${embedId}`}
-    frameBorder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-    allowFullScreen
-  />
-)
-
 const Video = ({ theAtom }: { theAtom: BlockAtom }) => {
   const block = useAtomValue(theAtom) as VideoBlockObjectResponse
-  const videoUrl: string =
-    block.video.type == 'external'
-      ? block.video.external.url
-      : block.video.file.url
+  const [provider, id] = useMemo(() => {
+    const videoUrl: string =
+      block.video.type == 'external'
+        ? block.video.external.url
+        : block.video.file.url
+    if (videoUrl.indexOf('youtube.com') === -1) {
+      return [null, videoUrl]
+    } else {
+      const url = new URL(videoUrl)
+      if (url.searchParams.has('v')) {
+        return ['youtube', url.searchParams.get('v') as string]
+      } else {
+        return ['youtube', url.pathname.split('/').filter(segment => segment !== '').pop() as string]
+      }
+    }
+  }, [block])
   return (
     <div className="notion_video_container">
-      {
-        videoUrl.indexOf('youtube.com') > -1 ? (
-          <video controls src={videoUrl} className={`notion_video`} />
-        ) : (
-          <YoutubeEmbed embedId={videoUrl.split('/').filter(segment => segment !== '').pop()} />
-        )
-      }
+      {provider === 'youtube' ? (<LiteYouTubeEmbed id={id} title="" />) : null}
+      {provider === null ? (<video controls src={id} className={`notion_video`} />) : null}
       <span className="notion_caption">
         {block.video.caption && <RichText rich_text={block.video.caption} />}
       </span>
