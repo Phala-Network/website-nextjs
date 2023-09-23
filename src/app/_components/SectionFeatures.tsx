af0d1d5f7c01cf4a933e0bb1a0153f22463b8b7a
Paco Yang <yangbaigao@gmail.com>
2023-09-23 15:46:32 +0800

    chore: styles adjustment

--- src/app/_components/SectionFeatures.tsx
+++ src/app/_components/SectionFeatures.tsx
@@ -13,18 +13,24 @@ function SectionFeatures({ list }: { list: PhatItem[][] }) {
       id="section-features"
       className={cn(
         "safe-viewport pt-12 lg:pt-32",
-        "flex flex-col gap-4 lg:gap-0",
-        "items-center"
+        "grid gap-4 grid-cols-2 lg:grid-cols-20 3xl:grid-cols-24 grid-rows-1",
       )}
     >
-      <div className="flex flex-col text-center">
-        <h2 className="font-normal leading-tight text-4xl lg:text-6xl">Say hello to</h2>
-        <h2 className="font-black leading-tight text-4xl lg:text-6xl">Phat Contract</h2>
-        <div>
-          <button className="mt-8 bg-black-800 text-white font-bold text-md py-3 px-7 rounded-full">Start Phat Contrat</button>
+      <div
+        className={cn(
+          "flex flex-col items-center gap-4 lg:gap-0",
+          "col-start-1 col-span-full lg:col-span-18 lg:col-start-2 3xl:col-start-4 3xl:col-span-18 row-start-1"
+        )}
+      >
+        <div className="flex flex-col text-center gap-2 2xl:gap-4">
+          <h2 className="font-normal leading-tight text-4xl 2xl:text-6xl">Say hello to</h2>
+          <h2 className="font-black leading-tight text-4xl 2xl:text-6xl">Phat Contract</h2>
+          <div className="flex items-center justify-center mt-4 lg:mt-6">
+            <button className="bg-black-800 text-white font-bold text-md py-3 px-7 rounded-full 2xl:px-10 2xl:py-6">Start Phat Contrat</button>
+          </div>
         </div>
+        <PhatContract />
       </div>
-      <PhatContract />
     </section>
   )
 }
