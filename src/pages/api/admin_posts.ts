137905f088d1ace6d22cf1ea695baf5a6b4ff40d
pacoyang <yangbaigao@gmail.com>
2023-11-09 13:35:26 +0800

    feat: admin page

---
+++ src/pages/api/admin_posts.ts
@@ -0,0 +1,43 @@
+import { NextApiResponse, NextApiRequest } from 'next'
+import { QueryDatabaseParameters } from '@notionhq/client/build/src/api-endpoints'
+
+import { queryDatabase } from '@/lib/notion-client'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { cursor, page_size = 20 } = req.query
+  const filter: QueryDatabaseParameters['filter'] = {
+    and: [
+      {
+        property: 'Post Type',
+        select: {
+          equals: 'Post',
+        },
+      },
+      {
+        property: 'Tags',
+        multi_select: {
+          does_not_contain: 'Changelog',
+        },
+      },
+    ],
+  }
+  const { next_cursor, pages } = await queryDatabase({
+    database_id: process.env.NOTION_POSTS_DATABASE_ID!,
+    filter,
+    sorts: [
+      {
+        property: 'Created Time',
+        direction: 'descending',
+      },
+    ],
+    page_size: parseInt(page_size as string, 10),
+    start_cursor: cursor as string,
+  })
+  return res.json({
+    pages,
+    next_cursor,
+  })
+}
