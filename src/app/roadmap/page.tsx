34c7bcd843b5ed9b7c2ca10d02aa1bb9257e1f7a
pacoyang <yangbaigao@gmail.com>
2024-01-27 15:23:04 +0800

    feat: add roadmap component

--- src/app/roadmap/page.tsx
+++ src/app/roadmap/page.tsx
@@ -1,8 +1,5 @@
 import { type Metadata } from 'next'
-import { cn } from "@/lib/utils"
-import DotBackground from '@/components/DotBackground'
-
-import './style.css'
+import Roadmap from '@/components/roadmap'
 
 export const metadata: Metadata = {
   title: 'Roadmap | Phala Network',
@@ -10,20 +7,6 @@ export const metadata: Metadata = {
 
 export default function Page() {
   return (
-    <div className="relative py-24">
-      <DotBackground dotColor="#E2E8F0" bgColor="#F7FAFC" />
-      <main className={cn("safe-viewport", "grid grid-cols-1 xl:grid-cols-20 3xl:grid-cols-24")}>
-        <article className={cn("cols-span-1 xl:col-start-2 xl:col-span-18 3xl:col-start-6 3xl:col-span-14 min-h-[60rem] pl-3 pr-2 py-4", "pageRoadmap")}>
-          <iframe
-            className="overflow-hidden rounded-xl"
-            width="100%"
-            height="100%"
-            src="https://miro.com/app/live-embed/uXjVN9U3y7A=/?moveToViewport=-1220,-982,4708,5928&embedId=609118477724"
-            allow="fullscreen; clipboard-read; clipboard-write"
-            allowFullScreen
-          />
-        </article>
-      </main>
-    </div>
+    <Roadmap />
   )
 }
---
+++ src/components/roadmap/index.tsx
@@ -0,0 +1,113 @@
+'use client'
+
+import { useRef, useLayoutEffect, useState } from 'react'
+import panzoom from 'panzoom'
+
+import { cn } from '@/lib/utils'
+import DotBackground from '@/components/DotBackground'
+import RoadmapSvg from './svg'
+import './style.css'
+
+export const Roadmap = () => {
+  const ref = useRef<any>(null)
+  const panzoomRef = useRef<any>(null)
+  const [isFullscreen, setFullscreen] = useState(false)
+
+  useLayoutEffect(() => {
+    panzoomRef.current = panzoom(ref.current, {
+      minZoom: 1,
+      maxZoom: 5,
+      bounds: true,
+      boundsPadding: .25,
+      zoomDoubleClickSpeed: 1,
+      beforeMouseDown: function() {
+        return !1
+      },
+      onTouch: function() {}
+    })
+
+    const handler = () => {
+      setFullscreen(!!document.fullscreenElement)
+    }
+
+    document.addEventListener('fullscreenchange', handler)
+
+    return () => {
+      panzoomRef.current.dispose()
+      document.removeEventListener('fullscreenchange', handler)
+    }
+  }, [])
+
+  const handleZoomin = () => {
+    panzoomRef.current.zoomTo(
+      ref.current.offsetWidth / 2,
+      ref.current.offsetHeight / 2,
+      1 + 0.5,
+    )
+  }
+
+  const handleZoomout = () => {
+    panzoomRef.current.zoomTo(
+      ref.current.offsetWidth / 2,
+      ref.current.offsetHeight / 2,
+      1 - 0.5,
+    )
+  }
+
+  const handleFullscreen = () => {
+    // @ts-ignore
+    if (document.fullscreenElement === null || document.webkitIsFullScreen === false) {
+      // @ts-ignore
+      document.documentElement.requestFullscreen ? document.documentElement.requestFullscreen() : document.documentElement.webkitRequestFullscreen ? document.documentElement.webkitRequestFullscreen() : document.documentElement.mozRequestFullScreen ? document.documentElement.mozRequestFullScreen() : document.documentElement.msRequestFullscreen && document.documentElement.msRequestFullscreen()
+    } else {
+      // @ts-ignore
+      document.exitFullscreen ? document.exitFullscreen() : document.webkitExitFullscreen ? document.webkitExitFullscreen() : document.mozCancelFullScreen && document.mozCancelFullScreen()
+    }
+  }
+
+  return (
+    <div className="relative">
+      <DotBackground dotColor="#E2E8F0" bgColor="#F7FAFC" />
+      <main className={cn("safe-viewport", "grid grid-cols-1 xl:grid-cols-20 3xl:grid-cols-24", "relative overflow-hidden py-24")}>
+        <article className={cn("cols-span-1 xl:col-start-2 xl:col-span-18 3xl:col-start-6 3xl:col-span-14 py-4")} ref={ref}>
+          <RoadmapSvg />
+        </article>
+        <div className="roadmap-tools pt-24">
+          <div className="roadmap-tools-group">
+            <button
+              className="roadmap-tool roadmap-tool-zoomin"
+              onClick={handleZoomin}
+            >
+              <svg viewBox="0 0 12 12">
+                <line x1="0" y1="6" x2="12" y2="6"></line>
+                <line x1="6" y1="0" x2="6" y2="12"></line>
+              </svg>
+            </button>
+            <button
+              className="roadmap-tool roadmap-tool-zoomout"
+              onClick={handleZoomout}
+            >
+              <svg viewBox="0 0 12 12">
+                <line x1="0" y1="6" x2="12" y2="6"></line>
+              </svg>
+            </button>
+          </div>
+          <div className="roadmap-tools-group">
+            <button
+              className={cn("roadmap-tool roadmap-tool-fullscreen", isFullscreen ? 'active' : '')}
+              onClick={handleFullscreen}
+            >
+              <svg viewBox="0 0 14 14">
+                <polyline points="1,5.2 1,1 5.2,1"></polyline>
+                <polyline points="8.8,1 13,1 13,5.2"></polyline>
+                <polyline points="13,8.8 13,13 8.8,13"></polyline> <polyline points="5.2,13 1,13 1,8.8"></polyline>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default Roadmap
