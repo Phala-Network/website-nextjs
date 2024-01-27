580e54431589c02d922af52a526f6c5845dbf33c
pacoyang <yangbaigao@gmail.com>
2024-01-27 17:49:02 +0800

    chore: update styles

--- src/components/roadmap/index.tsx
+++ src/components/roadmap/index.tsx
@@ -21,9 +21,8 @@ export const Roadmap = () => {
       boundsPadding: .25,
       zoomDoubleClickSpeed: 1,
       beforeMouseDown: function() {
-        return !1
+        return false
       },
-      onTouch: function() {}
     })
 
     const handler = () => {
@@ -66,46 +65,46 @@ export const Roadmap = () => {
   }
 
   return (
-    <div className="relative">
+    <div className="relative pb-24">
       <DotBackground dotColor="#E2E8F0" bgColor="#F7FAFC" />
-      <main className={cn("safe-viewport", "grid grid-cols-1 xl:grid-cols-20 3xl:grid-cols-24", "relative overflow-hidden py-24")}>
-        <article className={cn("cols-span-1 xl:col-start-2 xl:col-span-18 3xl:col-start-6 3xl:col-span-14 py-4")} ref={ref}>
+      <main className={cn("safe-viewport", "grid grid-cols-1 xl:grid-cols-20 3xl:grid-cols-24", "overflow-hidden")}>
+        <article className={cn("cols-span-1 xl:col-start-2 xl:col-span-18 3xl:col-start-6 3xl:col-span-14 pt-28")} ref={ref}>
           <RoadmapSvg />
         </article>
-        <div className="roadmap-tools pt-24">
-          <div className="roadmap-tools-group">
-            <button
-              className="roadmap-tool roadmap-tool-zoomin"
-              onClick={handleZoomin}
-            >
-              <svg viewBox="0 0 12 12">
-                <line x1="0" y1="6" x2="12" y2="6"></line>
-                <line x1="6" y1="0" x2="6" y2="12"></line>
-              </svg>
-            </button>
-            <button
-              className="roadmap-tool roadmap-tool-zoomout"
-              onClick={handleZoomout}
-            >
-              <svg viewBox="0 0 12 12">
-                <line x1="0" y1="6" x2="12" y2="6"></line>
-              </svg>
-            </button>
-          </div>
-          <div className="roadmap-tools-group">
-            <button
-              className={cn("roadmap-tool roadmap-tool-fullscreen", isFullscreen ? 'active' : '')}
-              onClick={handleFullscreen}
-            >
-              <svg viewBox="0 0 14 14">
-                <polyline points="1,5.2 1,1 5.2,1"></polyline>
-                <polyline points="8.8,1 13,1 13,5.2"></polyline>
-                <polyline points="13,8.8 13,13 8.8,13"></polyline> <polyline points="5.2,13 1,13 1,8.8"></polyline>
-              </svg>
-            </button>
-          </div>
-        </div>
       </main>
+      <div className="roadmap-tools pt-24">
+        <div className="roadmap-tools-group">
+          <button
+            className="roadmap-tool roadmap-tool-zoomin"
+            onClick={handleZoomin}
+          >
+            <svg viewBox="0 0 12 12">
+              <line x1="0" y1="6" x2="12" y2="6"></line>
+              <line x1="6" y1="0" x2="6" y2="12"></line>
+            </svg>
+          </button>
+          <button
+            className="roadmap-tool roadmap-tool-zoomout"
+            onClick={handleZoomout}
+          >
+            <svg viewBox="0 0 12 12">
+              <line x1="0" y1="6" x2="12" y2="6"></line>
+            </svg>
+          </button>
+        </div>
+        <div className="roadmap-tools-group">
+          <button
+            className={cn("roadmap-tool roadmap-tool-fullscreen", isFullscreen ? 'active' : '')}
+            onClick={handleFullscreen}
+          >
+            <svg viewBox="0 0 14 14">
+              <polyline points="1,5.2 1,1 5.2,1"></polyline>
+              <polyline points="8.8,1 13,1 13,5.2"></polyline>
+              <polyline points="13,8.8 13,13 8.8,13"></polyline> <polyline points="5.2,13 1,13 1,8.8"></polyline>
+            </svg>
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
