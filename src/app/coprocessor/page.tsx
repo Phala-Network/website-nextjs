1dca2699fd87319da94fe985ffd887ed2bc5dd7e
Leechael Yim <yanleech@gmail.com>
2024-02-22 17:15:26 +0800

    imp: ui adjustment

--- src/app/coprocessor/page.tsx
+++ src/app/coprocessor/page.tsx
@@ -45,7 +45,7 @@ const deepdives = [
 
 const challenges = [
   {
-    name: 'challenge 1 - Multi-Chain Deployment',
+    name: 'Challenge 1 - Multi-Chain Deployment',
     description: dedent`
       A key limitation of Multi-Chain smart contracts lies in their restricted interoperability across various blockchains, sidechains, and layer-2 networks.
     `,
@@ -182,20 +182,12 @@ export default function Page() {
           <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-6xl">
             <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
               {challenges.map((feature) => (
-                <div key={feature.name} className="flex flex-col">
-                  <dt className="text-base font-semibold leading-7 text-white">
-                    <div className="mb-6 flex h-10 w-10 items-center justify-center rounded-lg bg-indigo-500">
-                      {/* <feature.icon className="h-6 w-6 text-white" aria-hidden="true" /> */}
-                    </div>
+                <div key={feature.name} className="flex flex-col gap-4">
+                  <dt className="text-2xl font-bold leading-7 bg-gradient-to-br from-phalaPurple-400 to-phalaPurple-600 text-white p-4 rounded-full">
                     {feature.name}
                   </dt>
                   <dd className="mt-1 flex flex-auto flex-col text-base leading-7 text-gray-300">
                     <p className="flex-auto">{feature.description}</p>
-                    <p className="mt-6">
-                      <a href={feature.href} className="text-sm font-semibold leading-6 text-indigo-400">
-                        Learn more <span aria-hidden="true">→</span>
-                      </a>
-                    </p>
                   </dd>
                 </div>
               ))}
