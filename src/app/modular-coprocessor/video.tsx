7acdbd17f98247000d49433242b87d19911f7d87
Leechael Yim <yanleech@gmail.com>
2024-03-06 22:43:45 +0800

    fix: temporary fix on video

---
+++ src/app/modular-coprocessor/video.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import LiteYouTubeEmbed from 'react-lite-youtube-embed'
+import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css'
+
+export function Video() {
+  return (
+    <LiteYouTubeEmbed
+      id="Pjhz0_ROpBM"
+      title=""
+      poster="sddefault"
+    />
+  )
+}
