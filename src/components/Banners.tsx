4e5a67204efae4ce5125f49985d2d0bafaf9c7fd
Leechael Yim <yanleech@gmail.com>
2024-04-17 01:22:58 +0800

    imp: hotfix for the post only

--- src/components/Banners.tsx
+++ src/components/Banners.tsx
@@ -50,8 +50,11 @@ export default function Banners({ pages }: { pages: ParsedListPage[] }) {
                     className="w-full aspect-[872/487]"
                     width={872}
                     height={487}
-                    // src={`https://img0.phala.world/notion/resize:fill:1744:974:0/plain/https://img0.phala.world/cover/${page.id}.jpg`}
-                    src={`https://img0.phala.world/cover/${page.id}.jpg`}
+                    src={
+                      page.id === '879ccad7-3aaf-4c7e-b043-d98a1b77ee7b'
+                        ? `https://img0.phala.world/cover/${page.id}.jpg`
+                        : `https://img0.phala.world/notion/resize:fill:1744:974:0/plain/https://img0.phala.world/cover/${page.id}.jpg`
+                    }
                   />
                 </a>
               </div>
