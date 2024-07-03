633dfe893b646c79c51ec201978a6f6d7a63efad
Leechael Yim <yanleech@gmail.com>
2024-07-04 00:50:07 +0800

    feat: setup canonical link for all pages.

--- src/app/roadmap/page.tsx
+++ src/app/roadmap/page.tsx
@@ -3,6 +3,9 @@ import Roadmap from '@/components/roadmap'
 
 export const metadata: Metadata = {
   title: 'Roadmap | Phala Network',
+  alternates: {
+    canonical: "https://phala.network/roadmap",
+  }
 }
 
 export default function Page() {
