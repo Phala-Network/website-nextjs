f201a34efc5f6aea677b1abb68449429970aaa03
Leechael Yim <yanleech@gmail.com>
2024-07-08 20:27:45 +0800

    fix: use route.ts instead page.tsx

--- src/app/rss.xml/route.ts
+++ src/app/rss.xml/route.ts
@@ -1,6 +1,6 @@
 import { getBlogFeed } from '@/lib/rss_feed'
 
-export default async function Page() {
+export async function GET() {
   const feed = await getBlogFeed()
   return new Response(
     feed.rss2(),
