e9cd025ee2ff6c199cd049503e89a576d515813b
kingsleydon <10992364+kingsleydon@users.noreply.github.com>
2025-04-10 18:54:20 +0800

    remove khala

--- src/queries/GetComputationData.ts
+++ src/queries/GetComputationData.ts
@@ -1,17 +1,10 @@
 const PHALA_PARA_ID = 2035
-const KHALA_PARA_ID = 2004
 const PHALA_METADATA_ENDPOINT =
   'https://phala.webapi.subscan.io/api/scan/metadata'
-const KHALA_METADATA_ENDPOINT =
-  'https://khala.webapi.subscan.io/api/scan/metadata'
 const POLKADOT_XCM_ENDPOINT =
   'https://polkadot.api.subscan.io/api/scan/parachain/info'
-const KUSAMA_XCM_ENDPOINT =
-  'https://kusama.api.subscan.io/api/scan/parachain/info'
 const PHALA_SQUID_ENDPOINT =
   'https://subsquid.phala.network/phala-computation/graphql'
-const KHALA_SQUID_ENDPOINT =
-  'https://subsquid.phala.network/khala-computation/graphql'
 
 interface Xcm {
   data: {
@@ -89,20 +82,10 @@ const fetchSquid = async (endpoint: string) =>
     })
 
 export async function getComputationData() {
-  const [
-    phalaXcm,
-    khalaXcm,
-    phalaMetadata,
-    khalaMetadata,
-    phalaSquid,
-    khalaSquid,
-  ] = await Promise.all([
+  const [phalaXcm, phalaMetadata, phalaSquid] = await Promise.all([
     fetchXcm(POLKADOT_XCM_ENDPOINT, PHALA_PARA_ID),
-    fetchXcm(KUSAMA_XCM_ENDPOINT, KHALA_PARA_ID),
     fetchMetadata(PHALA_METADATA_ENDPOINT),
-    fetchMetadata(KHALA_METADATA_ENDPOINT),
     fetchSquid(PHALA_SQUID_ENDPOINT),
-    fetchSquid(KHALA_SQUID_ENDPOINT),
   ])
 
   const data = {
@@ -114,31 +97,15 @@ export async function getComputationData() {
     phalaIdleWorkerCount: phalaSquid.idleWorkerCount,
     phalaWorkerCount: phalaSquid.workerCount,
     phalaTotalValue: phalaSquid.totalValue,
-    khalaSendXcmMessageCount: khalaXcm.xcm_send_message_count,
-    khalaReceivedXcmMessageCount: khalaXcm.xcm_receive_message_count,
-    khalaSignedExtrinsicCount: khalaMetadata.count_signed_extrinsic,
-    khalaIdlePInit: khalaSquid.idleWorkerPInit,
-    khalaIdlePInstant: khalaSquid.idleWorkerPInstant,
-    khalaIdleWorkerCount: khalaSquid.idleWorkerCount,
-    khalaWorkerCount: khalaSquid.workerCount,
-    khalaTotalValue: khalaSquid.totalValue,
   }
 
-  const totalValue =
-    Number.parseInt(data.phalaTotalValue) +
-    Number.parseInt(data.khalaTotalValue)
-  const totalNodes = data.phalaIdleWorkerCount + data.khalaIdleWorkerCount
-  const onlineWorkers = data.phalaIdleWorkerCount + data.khalaIdleWorkerCount
-  const vCpu = Math.floor((data.phalaIdlePInit + data.khalaIdlePInit) / 150)
+  const totalValue = Number.parseInt(data.phalaTotalValue)
+  const totalNodes = data.phalaIdleWorkerCount
+  const onlineWorkers = data.phalaIdleWorkerCount
+  const vCpu = Math.floor(data.phalaIdlePInit / 150)
   const crossChainTx =
-    data.phalaSendXcmMessageCount +
-    data.khalaSendXcmMessageCount +
-    data.phalaReceivedXcmMessageCount +
-    data.khalaReceivedXcmMessageCount
-  const tx = Number(
-    BigInt(data.phalaSignedExtrinsicCount) +
-      BigInt(data.khalaSignedExtrinsicCount)
-  )
+    data.phalaSendXcmMessageCount + data.phalaReceivedXcmMessageCount
+  const tx = Number(BigInt(data.phalaSignedExtrinsicCount))
 
   return {
     totalValue,
