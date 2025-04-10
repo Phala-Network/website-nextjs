af36862528622026117f8f477978c31ca4c6818e
Leechael Yim <yanleech@gmail.com>
2025-04-10 22:28:41 +0800

    cont.

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -151,7 +151,7 @@ const PostPage = ({
                     src={`https://img0.phala.world/cover/1744x974/${page.id.replace(
                       /\-/g,
                       ''
-                    )}.jpg`}
+                    )}.jpg?z=123`}
                     // src={`https://img0.phala.world/notion/resize:fill:1744:974:0/plain/https://img0.phala.world/cover/${page.id}.jpg`}
                     // src={`https://img0.phala.world/cover/${page.id}.jpg`}
                     // src={
