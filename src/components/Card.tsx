071ee7eb634ee8728947b98137b8cded7c20a6e3
Leechael Yim <yanleech@gmail.com>
2025-07-16 01:59:31 +0800

    chore: image

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -4,6 +4,7 @@ import { ParsedListPage } from '@/lib/notion-client'
 
 const remap: Readonly<Record<string, string>> = {
   '2250317e04a18058a89af73b666d10e0': '2250317e-04a1-8058-a89a-f73b666d10e0',
+  '2300317e04a18074a132f0b95e4cc4d5': '2300317e-04a1-8074-a132-f0b95e4cc4d5',
 }
 
 export default function Card({ page }: { page: ParsedListPage }) {
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -24,6 +24,7 @@ import '@/components/notion-render/styles.css'
 
 const remap: Readonly<Record<string, string>> = {
   '2250317e04a18058a89af73b666d10e0': '2250317e-04a1-8058-a89a-f73b666d10e0',
+  '2300317e04a18074a132f0b95e4cc4d5': '2300317e-04a1-8074-a132-f0b95e4cc4d5',
 }
 
 interface Props {
