057e033c6fd437382a5fe784622f8b6147ec56fd
kingsleydon <10992364+kingsleydon@users.noreply.github.com>
2025-08-08 21:19:52 -0700

    remove files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,67 @@
 import { GoogleTagManager } from '@next/third-parties/google'
+import type { Metadata, Viewport } from 'next'
+import { Inter, Montserrat, Open_Sans } from 'next/font/google'
 import Script from 'next/script'
 
+import Footer from '@/components/footer'
+import Nav from '@/components/nav'
 import ScrollToTop from '@/components/ScrollToTop'
-import SiteFooter from '@/components/SiteFooter'
-import SiteNav from '@/components/SiteNav'
-import fontVariables from '@/lib/fonts'
+import { cn } from '@/lib/utils'
 
-import 'swiper/css'
-import 'swiper/css/free-mode'
 import './globals.css'
+
+export const viewport: Viewport = {
+  themeColor: 'rgba(232, 233, 234, 1)',
+}
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Phala',
+    default: 'Phala: The New Cloud for Confidential AI',
+  },
+  description:
+    'Phala is the new cloud for confidential AI helping build AI people can trust.',
+  keywords: [
+    'Phala',
+    'Confidential AI',
+    'TEE',
+    'GPU TEE',
+    'Confidential Computing',
+    'Web3',
+    'Verifiable Compute',
+    'Phala Network',
+  ],
+  twitter: {
+    site: '@PhalaNetwork',
+  },
+}
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' })
+const montserrat = Montserrat({
+  subsets: ['latin'],
+  variable: '--font-montserrat',
+})
+const openSans = Open_Sans({ subsets: ['latin'], variable: '--font-open-sans' })
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  // if (process.env.NODE_ENV === 'production') {
+  //   notFound()
+  // }
+
   return (
-    <html lang="en" className={fontVariables}>
-      {process.env.NEXT_PUBLIC_GTM_ID ? (
+    <html
+      lang="en"
+      className={cn(inter.variable, montserrat.variable, openSans.variable)}
+    >
+      {process.env.NEXT_PUBLIC_GTM_ID && (
         <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID} />
-      ) : null}
+      )}
       <body>
-        {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID ? (
+        {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
           <>
             <Script
               src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
@@ -37,10 +77,10 @@ export default function RootLayout({
               `}
             </Script>
           </>
-        ) : null}
+        )}
         {children}
-        {/* <SiteNav />
-        <SiteFooter /> */}
+        <Nav />
+        <Footer />
         <ScrollToTop />
       </body>
     </html>
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -31,6 +31,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         </>
       ) : null}
       {children}
+      <SiteNav />
       <SiteFooter />
       <ScrollToTop />
     </div>
