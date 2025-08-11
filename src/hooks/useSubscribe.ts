c34451759845cc91a75414457a018bc1fc12f407
kingsleydon <10992364+kingsleydon@users.noreply.github.com>
2025-08-10 21:35:34 -0700

    fix subscribe

--- src/hooks/useSubscribe.ts
+++ src/hooks/useSubscribe.ts
@@ -1,12 +1,14 @@
-import { useCallback } from 'react';
-import { atom, useAtom, useSetAtom, useAtomValue } from 'jotai';
+import { useCallback, useState } from 'react'
 
 // Poorman's Either
-export type SendPostFormResult = { succeed: true; message: string } | { succeed: false; error: string };
+export type SendPostFormResult =
+  | { succeed: true; message: string }
+  | { succeed: false; error: string }
 
 // @see https://emailregex.com/
 const regexEmail =
-  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+  // eslint-disable-next-line no-control-regex
+  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
 
 async function submit(email: string): Promise<SendPostFormResult> {
   try {
@@ -17,69 +19,71 @@ async function submit(email: string): Promise<SendPostFormResult> {
       },
       body: JSON.stringify({ email }),
     })
+
     if (resp.status >= 200 && resp.status < 300) {
-      return { succeed: true, message: 'Subscribed.' };
+      return { succeed: true, message: 'Subscribed.' }
     }
+
     if (resp.status === 400) {
-      const body = await resp.json();
-      return { succeed: false, error: body.message };
+      const body = await resp.json()
+      return { succeed: false, error: body.message }
     }
-    return { succeed: false, error: 'Unknown Error, please try again later.' };
+
+    return { succeed: false, error: 'Unknown Error, please try again later.' }
   } catch (err) {
-    console.error('Unexpected exception:', err);
-    return { succeed: false, error: 'Unknown Error, please try again later.' };
+    console.error('Unexpected exception:', err)
+    return { succeed: false, error: 'Unknown Error, please try again later.' }
   }
 }
 
-const fieldEmailValueAtom = atom('');
-const loadingAtom = atom(false);
-const messageAtom = atom('');
-const errorAtom = atom('');
-const isSucceedAtom = atom(false);
-const isErrorAtom = atom(false);
+export const useSubscribe = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [message, setMessage] = useState('')
+  const [error, setError] = useState('')
+  const [isSucceed, setIsSucceed] = useState(false)
+  const [isError, setIsError] = useState(false)
 
-export const subscribeDerivedAtom = atom(null, async (get, set) => {
-  const email = get(fieldEmailValueAtom);
-  if (!regexEmail.test(email)) {
-    set(isErrorAtom, true);
-    set(errorAtom, 'Please enter a valid email address.');
-    setTimeout(() => {
-      set(isErrorAtom, false);
-    }, 5000);
-    return;
-  }
-  set(loadingAtom, true);
-  // const result = await sendPostFormRequest(portalId, formId, email);
-  const result = await submit(email)
-  set(loadingAtom, false);
-  if (result.succeed) {
-    set(isSucceedAtom, true);
-    set(messageAtom, result.message);
-    setTimeout(() => {
-      set(isSucceedAtom, false);
-    }, 3000);
-  } else {
-    set(isErrorAtom, true);
-    set(errorAtom, result.error!);
-    setTimeout(() => {
-      set(isErrorAtom, false);
-    }, 5000);
-  }
-});
+  const onSubmit = useCallback(async (email: string) => {
+    if (!regexEmail.test(email)) {
+      setIsError(true)
+      setError('Please enter a valid email address.')
+      setTimeout(() => {
+        setIsError(false)
+      }, 5000)
+      return
+    }
 
-export const useSubscribe = function () {
-  const setEmail = useSetAtom(fieldEmailValueAtom);
-  const onSubmit = useSetAtom(subscribeDerivedAtom);
-  const isLoading = useAtomValue(loadingAtom);
-  const message = useAtomValue(messageAtom);
-  const error = useAtomValue(errorAtom);
-  const [isSucceed, setIsSucceed] = useAtom(isSucceedAtom);
-  const [isError, setIsError] = useAtom(isErrorAtom);
+    setIsLoading(true)
+    const result = await submit(email)
+    setIsLoading(false)
+
+    if (result.succeed) {
+      setIsSucceed(true)
+      setMessage(result.message)
+      setTimeout(() => {
+        setIsSucceed(false)
+      }, 3000)
+    } else {
+      setIsError(true)
+      setError(result.error)
+      setTimeout(() => {
+        setIsError(false)
+      }, 5000)
+    }
+  }, [])
 
   const dismiss = useCallback(() => {
     setIsError(false)
     setIsSucceed(false)
-  }, [setIsError, setIsSucceed])
+  }, [])
 
-  return { setEmail, onSubmit, isLoading, message, error, isSucceed, isError, dismiss };
-};
+  return {
+    onSubmit,
+    isLoading,
+    message,
+    error,
+    isSucceed,
+    isError,
+    dismiss,
+  }
+}
