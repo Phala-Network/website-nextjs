b196c04acbbc2d283f03458ce0f59831ac1fc9b4
kingsleydon <10992364+kingsleydon@users.noreply.github.com>
2025-08-11 07:22:15 -0700

    contact and mdx pages

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,16 @@
+import createMDX from '@next/mdx'
 import type { NextConfig } from 'next'
 
 import redirects from './redirects'
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   redirects,
 }
 
-export default nextConfig
+const withMDX = createMDX({
+  extension: /\.(md|mdx)$/,
+})
+
+export default withMDX(nextConfig)
