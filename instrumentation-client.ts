b7ad135540047107b5bfd1db60fd397306145924
kingsleydon <10992364+kingsleydon@users.noreply.github.com>
2025-08-12 21:09:48 -0700

    chore: update deps

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,10 +1,14 @@
 import posthog from 'posthog-js'
 
-// Initialize PostHog client with defaults
-posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: '/ingest',
-  ui_host: 'https://us.posthog.com',
-  defaults: '2025-05-24',
-  capture_exceptions: true, // enable capturing exceptions via Error Tracking
-  debug: process.env.NODE_ENV === 'development',
-})
+import { env } from '@/env'
+
+if (env.NEXT_PUBLIC_POSTHOG_KEY) {
+  // Initialize PostHog client with defaults
+  posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: '/relay-ph',
+    ui_host: env.NEXT_PUBLIC_POSTHOG_HOST,
+    defaults: '2025-05-24',
+    capture_exceptions: true, // enable capturing exceptions via Error Tracking
+    debug: process.env.NODE_ENV === 'development',
+  })
+}
