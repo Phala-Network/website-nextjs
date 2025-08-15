eb6e0ed71913a736852eb9d471cb729034c3258b
Leechael Yim <yanleech@gmail.com>
2025-08-16 02:56:10 +0800

    fix: cover remap

--- src/lib/post-client.ts
+++ src/lib/post-client.ts
@@ -1,5 +1,5 @@
 export const coverRemap: Readonly<Record<string, string>> = {
   '2250317e04a18058a89af73b666d10e0': '2250317e-04a1-8058-a89a-f73b666d10e0',
   '2300317e04a18074a132f0b95e4cc4d5': '2300317e-04a1-8074-a132-f0b95e4cc4d5',
-  '24d0317e04a1807680eade2f2dead3cd': '24d0317e04a1807680eade2f2dead3cd-2',
+  '24d0317e04a1807680eade2f2dead3cd': '24d0317e-04a1-8076-80ea-de2f2dead3cd',
 }
