bcac3dd3738139e1b9e7a608e34161cf02ca89ab
kingsleydon <10992364+kingsleydon@users.noreply.github.com>
2025-09-17 20:25:09 -0700

    feat: add metadata for About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from 'next'
+
 import About from './about'
+
+export const metadata: Metadata = {
+  title: 'About Us',
+}
 
 export default function AboutPage() {
   return <About />
