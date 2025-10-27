581a32db5a84c018779c9b8a40a64fb386b7ff12
h4x3rotab <h4x3rotab@gmail.com>
2025-10-27 11:53:53 -0700

    Update disallow rules for robots.txt
    Fix 404 caused by cloudflare email protection

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/api/',
+      disallow: ['/api/', '/cdn-cgi/'],
     },
     sitemap: `https://${env.VERCEL_PROJECT_PRODUCTION_URL}/sitemap.xml`,
   }
